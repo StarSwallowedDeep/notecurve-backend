3b4274ac99b555b6ea9dbef69de2cfbdf46c56a3
StarSwallowedDeep <139122242+StarSwallowedDeep@users.noreply.github.com>
2025-12-20 20:38:04 +0900

    refactor(User): organize import order and rearrange annotations

--- src/main/java/com/notecurve/user/repository/UserRepository.java
+++ src/main/java/com/notecurve/user/repository/UserRepository.java
@@ -1,7 +1,7 @@
 package com.notecurve.user.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 import com.notecurve.user.domain.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
