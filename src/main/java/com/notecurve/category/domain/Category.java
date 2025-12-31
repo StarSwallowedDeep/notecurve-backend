a4c728322fd4d811ec072334cab2ff19ef994216
StarSwallowedDeep <139122242+StarSwallowedDeep@users.noreply.github.com>
2025-12-31 20:41:37 +0900

    refactor(category): small code clean-up in category module

--- src/main/java/com/notecurve/category/domain/Category.java
+++ src/main/java/com/notecurve/category/domain/Category.java
@@ -15,7 +15,6 @@ import com.notecurve.user.domain.User;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 @Entity
 public class Category {
 
@@ -31,10 +30,4 @@ public class Category {
     // 카테고리에 연결된 노트들
     @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Note> notes = new ArrayList<>();
-
-    // 카테고리 이름과 사용자만을 받아 초기화하는 생성자
-    public Category(String name, User user) {
-        this.name = name;
-        this.user = user;
-    }
 }
