a1d1f736084a4ae27e32f2a9dca28d4236b862d7
StarSwallowedDeep <139122242+StarSwallowedDeep@users.noreply.github.com>
2026-01-02 20:02:32 +0900

    refactor(note): convert NoteSummaryDTO to record

--- src/main/java/com/notecurve/note/dto/NoteSummaryDTO.java
+++ src/main/java/com/notecurve/note/dto/NoteSummaryDTO.java
@@ -1,11 +1,3 @@
 package com.notecurve.note.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
-public class NoteSummaryDTO {
-    private final Long id;
-    private final String title;
-}
+public record NoteSummaryDTO(Long id, String title) {}
